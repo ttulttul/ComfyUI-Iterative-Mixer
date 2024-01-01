c8177c0cc11d9ba954c4496da22e34ffd595a0b6
Ken Simpson <ksimpson@mailchannels.com>
2023-12-31 20:57:47 -0800

    Classified the mixing function to ease calling by different nodes.

--- nodes.py
+++ nodes.py
@@ -20,6 +20,31 @@ def _trace(msg):
     import logging as logger
     logger.warning(msg)
 
+def _trace_params(func):
+    import inspect
+    import collections.abc
+
+    def shorten(arg):
+        if hasattr(arg, "shape") and hasattr(arg, "dtype"):
+            return f"tensor(dtype={arg.dtype}, shape={arg.shape}, max={torch.max(arg)}, min={torch.min(arg)})"
+        elif isinstance(arg, str):
+            return f"'{arg}'"
+        elif isinstance(arg, collections.abc.Mapping):
+            return "dict{%s}" % (', '.join(f'{k}=self' if v is arg else f'{k}={shorten(v)}' for k, v in arg.items()))
+        elif isinstance(arg, collections.abc.Iterable):
+            return "list{%s}" % (', '.join(f'{i}=self' if el is arg else f'{i}={shorten(el)}' for i, el in enumerate(arg)))
+        else:
+            return f"{type(arg)}:{repr(arg)}"
+
+    def wrapper(*args, **kwargs):
+        bound_args = inspect.signature(func).bind(*args, **kwargs)
+        bound_args.apply_defaults()
+
+        args_str = ",\n".join([f'  {name}={shorten(value)}' for name, value in bound_args.arguments.items()])
+        _trace(f"{func.__name__}(\n{args_str})")
+        return func(*args, **kwargs)
+    return wrapper
+
 def _generate_class_map(cls: type, name_attribute: str) -> Dict[str, type]:
     """
     Generate a dict mapping from strings to classes. Each class should
@@ -458,7 +483,6 @@ class BatchUnsampler:
 
     CATEGORY = "tests"
 
-
     def unsampler(self, model, sampler_name, scheduler, steps,
                   start_at_step, end_at_step, latent_image, normalize=False):
         """
@@ -547,6 +571,230 @@ def get_blending_schedule(indices: list[int], blending_schedule: str, **kwargs):
 
 def get_blending_function(blending_function):
     return BLENDING_FUNCTION_MAP[blending_function]()
+
+class IterativeMixingKSampler:
+    """
+    A class to manage all the pre-processing of arguments that we need before we
+    can conduct sampling. This class exists to make it easier for us to develop
+    derivative algorithms and play with ideas without rewriting everything each time.
+    """
+
+    @_trace_params
+    def preamble(self, model, seed, cfg, sampler_name, scheduler, positive, negative,
+                 latent_image_batch, denoise=1.0, disable_noise=False,
+                 force_full_denoise=False, c1=None, alpha_1=0.5, reverse_input_batch=True,
+                 blending_schedule="cosine",
+                 start_blending_at_pct=0.0,
+                 stop_blending_at_pct=1.0,
+                 clamp_blending_at_pct=1.0,
+                 blending_function="addition"):
+        # Store basic parameters.
+        self.model = model
+        self.cfg = cfg
+        self.sampler_name = sampler_name
+        self.scheduler = scheduler
+        self.positive = positive
+        self.negative = negative
+        self.denoise = denoise
+        self.force_full_denoise = force_full_denoise
+        self.disable_noise = disable_noise
+        self.seed = seed
+
+        # I originally had a step_increment argument but there is no purpose
+        # to it, because you can just set the step count and of course
+        # the sigmas and everything will adjust accordingly.
+        self.step_increment = 1
+
+        self.z_primes = latent_image_batch["samples"]
+
+        # You will almost always want to reverse the input batch, which
+        # we assume is a set of progressively noisier latents. For de-noising,
+        # this batch order needs to be reversed so that we're going from
+        # the noisiest latent back to the start.
+        if reverse_input_batch == True:
+            self.z_primes = torch.flip(self.z_primes, [0])
+
+        if disable_noise:
+            self.noise = torch.zeros(self.z_primes.size(),
+                                     dtype=self.z_primes.dtype,
+                                     layout=self.z_primes.layout, device="cpu")
+        else:
+            self.batch_inds = latent_image_batch["batch_index"] if "batch_index" in latent_image_batch else None
+            self.noise = comfy.sample.prepare_noise(self.z_primes, seed, self.batch_inds)
+
+        self.noise_mask = None
+        if "noise_mask" in latent_image_batch:
+            self.noise_mask = latent_image_batch["noise_mask"]
+
+        # Assume that the first image in the batch is the most noised,
+        # representing z_prime[T] from the DemoFusion paper. You may
+        # need to reverse the batch of latents to ensure that the first
+        # latent in the batch is the most noised.
+
+        # Set up a tensor to receive the samples from the de-noising process.
+        # It has the same shape as z_primes and the first element is
+        # the first element of z_primes. We do some fancy footwork here
+        # to give z_out the right batch count given the step_increment
+        # parameter.
+
+        # Define the indices of z_prime that we will be visiting.
+        # We start with z_prime[0 + step_increment] and go up in steps of
+        # step_increment. We make sure we visit z_prime[-1] if the step_increment
+        # is such that we would otherwise end short of it.
+        self.steps = self.z_primes.shape[0] # batch size gives us the de-noising step count
+        self.zp_indices = list(range(self.step_increment - 1, self.steps, self.step_increment))
+        if self.zp_indices[-1] != self.steps - 1:
+            self.zp_indices.append(self.steps - 1)
+
+        self.loop_count = len(self.zp_indices)
+
+        # z_out is going to store z_prime[0] plus all the de-noised
+        # weighted mixtures of denoise(z_prime[i], z_hat[i-1]).
+        # Therefore, we give it a length that is one longer than
+        # the loop count because the first entry is set to z_prime[0].
+        # z_out has the same channels, width, and height as z_prime.
+        self.z_out = torch.zeros(self.loop_count + 1, *self.z_primes.size()[1:])
+        self.z_out = self.z_out.to(self.z_primes.device)
+
+        # We also return the intermediate samples for analysis.
+        # This tensor has the same shape exactly as z_out but the
+        # first entry is left all zeroes.
+        self.samples_out = torch.zeros_like(self.z_out)
+        self.samples_out = self.samples_out.to(self.z_primes.device)
+
+        # The first output value is the first z_prime latent.
+        # Implicitly, samples_out[0] is just zeroes.
+        self.z_out[0] = self.z_primes[0]
+
+        # The first z_prime that we will use in the de-noising loop below
+        # is z_primes[0] unsqueezed to remove the batch dimension.
+        self.z_i = self.z_primes[0].unsqueeze(0)
+
+
+        # If desired, start blending at a certain step count by setting
+        # the blending curve to 0.0
+        self.start_blending_at_step = int(self.steps * start_blending_at_pct)
+
+        # If desired, stop blending at a certain fraction of steps by
+        # setting the last few values of c1 to 1.0, which will cause
+        # 0% of the z_prime latents to be mixed at each following step.
+        # This setting can help to reduce the noisiness of the output at
+        # the expense of reducing guidance slightly.
+        self.stop_blending_at_step = int(self.steps * stop_blending_at_pct)
+
+        # Get the blending parameter from the DemoFusion paper.
+        self.c1 = get_blending_schedule(self.zp_indices, blending_schedule, alpha_1=alpha_1,
+                                        start_blending_at_step=self.start_blending_at_step,
+                                        stop_blending_at_step=self.stop_blending_at_step,
+                                        clamp_blending_at_pct=clamp_blending_at_pct)
+
+        # Plot the blending schedule so we can return it in the advanced module.
+        self.blending_plot = plot_blending_schedule(self.c1)
+
+        # Move the blending schedule tensor to the same device as our
+        # latents.
+        self.c1 = self.c1.to(self.z_primes.device)
+
+        # Get a blending function.
+        self.blend = get_blending_function(blending_function)
+
+        # The paper suggests that we de-noise the image step by step, blending
+        # in samples from the noised z_hat tensor along the way according to 
+        # a blending schedule given by the c1 tensor. Each successively de-noised
+        # sample z[i] is given by this formula:
+        #
+        # z[i] = denoise(z[i-1]) * (1 - c1[i]) + z_prime[i-1] * c1
+        #
+        # Thus we have to do the following:
+        # a) latent_image contains the z_primes for all steps.
+        # b) Iterate through all the steps, denoising from z_prime[0] initially.
+        # c) Blend the denoised z[i] with z_prime[i] to get a new z[i].
+
+        self.pbar = comfy.utils.ProgressBar(self.steps)
+        self.disable_pbar = not comfy.utils.PROGRESS_BAR_ENABLED
+
+        # Set up our output variables.
+        self.out = latent_image_batch.copy()
+        self.z_primes_out = latent_image_batch.copy()
+        self.intermediates_out = latent_image_batch.copy()
+
+    def __call__(self, *args, **kwargs):
+        """
+        This method is meant to be called from the Node class and takes all the parameters
+        consumed by the preamble() method above. We don't consume those parameters directly;
+        instead we rely on preamble() to turn them into relevant instance attributes.
+        """
+        self.preamble(*args, **kwargs)
+        
+        for zp_idx, i in tqdm(enumerate(self.zp_indices)):
+            out_i = zp_idx + 1
+            
+            # Grab the i-th z_prime and i-th noise tensor from their batches.
+            # Unsqueezing replaces the batch dimension with 1, so it transforms
+            # [i, channel, width, height] into [1, channel, width, height]
+            z_prime_i = self.z_primes[i].unsqueeze(0)
+            noise_i = self.noise[i].unsqueeze(0)
+
+            # The paper tells us to de-noise z[i-1] from step
+            # T to T-1; in ComfyUI lingo, that means going from
+            # step i-1 to step i because we iterate in the reverse
+            # direction.
+            z_start_step = i - 1
+            z_last_step = i
+            z_i_minus_1 = self.z_out[out_i - 1]
+
+            # Define a callback function for the sampler that will
+            # correctly indicate our progress across the whole batch.
+            def inner_callback (step, x0, x, total_steps):
+                self.pbar.update_absolute(i + step + 1, total_steps * self.steps)
+
+            # De-noise z[i-1] from step i-1 to step i. Recall that since we
+            # start this loop from i=1, z[i-1] is initialized with z_prime[0].
+            # After we have the de-noised latent, we will mix it with z_prime[i]
+            # according to the paper's cosine blending function. The blended
+            # latent will then become z[i] and we will head to the next iteration.
+            samples_i = comfy.sample.sample(
+                            self.model, noise_i, self.steps, self.cfg,
+                            self.sampler_name, self.scheduler, self.positive, self.negative, z_i_minus_1,
+                            denoise=self.denoise,
+                            disable_noise=self.disable_noise,
+                            start_step=z_start_step,
+                            last_step=z_last_step,
+                            force_full_denoise=self.force_full_denoise,
+                            noise_mask=self.noise_mask,
+                            disable_pbar=True, seed=self.seed,
+                            callback=inner_callback)
+
+            # Move samples to the same device as z_prime_i so that we can
+            # work with them both to mix below.
+            samples_i = samples_i.to(z_prime_i.device)
+
+            # Store the de-noised samples in our output tensor.
+            self.samples_out[out_i] = samples_i
+
+            # Find z_hat (as per the paper) by applying the c1 blending schedule
+            # to the samples and the prior z_prime latent. The paper suggests 
+            # following this formula, which will mix in a declining fraction of
+            # z_prime as de-noising continues:
+            #
+            # z_hat[i] = denoise(z[i-1]) * (1 - c1[i]) + z_prime[i-1] * c1
+
+            c1_i = self.c1[zp_idx]
+            z_i = self.blend(z_prime_i, samples_i, c1_i)
+
+            self.z_out[out_i] = z_i
+
+        
+        self.out["samples"] = self.z_out
+        self.z_primes_out["samples"] = self.z_primes
+        self.intermediates_out["samples"] = self.samples_out
+
+        # We output three latent batches so that you can see how the process
+        # works step by step if you wish:
+        # 1. The de-noised latents.
+        # 2. The noised latents that were provided at the input.
+        # 3. The intermediate samples before mixing.
+        return (self.out, self.z_primes_out, self.intermediates_out, self.blending_plot)
 
 def iterative_mixing_ksampler(model, seed, cfg, sampler_name, scheduler, positive, negative,
                               latent_image_batch, denoise=1.0, disable_noise=False,
@@ -792,7 +1040,26 @@ class IterativeMixingKSamplerAdv:
                blending_schedule="cosine",
                stop_blending_at_pct=1.0, clamp_blending_at_pct=1.0,
                blending_function=list(BLENDING_FUNCTION_MAP.keys())[0]):
-        return iterative_mixing_ksampler(model, seed, cfg, sampler_name, scheduler, positive, negative,
+        sampler = IterativeMixingKSampler()
+        return sampler(model, seed, cfg, sampler_name, scheduler, positive, negative,
+                                         latent_image_batch, denoise=denoise, alpha_1=alpha_1, reverse_input_batch=True,
+                                         blending_schedule=blending_schedule,
+                                         stop_blending_at_pct=stop_blending_at_pct,
+                                         clamp_blending_at_pct=clamp_blending_at_pct,
+                                         blending_function=blending_function)
+
+class IterativeMixingKSamplerInline(IterativeMixingKSamplerAdv):
+    """
+    A version of the iterative mixing advanced node that uses the built-in callback
+    function of ComfyUI's sampler system. This is experimental.
+    """
+    def sample(self, model, seed, cfg, sampler_name, scheduler, positive, negative,
+               latent_image_batch, denoise=1.0, alpha_1=0.1, reverse_input_batch=True,
+               blending_schedule="cosine",
+               stop_blending_at_pct=1.0, clamp_blending_at_pct=1.0,
+               blending_function=list(BLENDING_FUNCTION_MAP.keys())[0]):
+        sampler = IterativeMixingKSampler()
+        return sampler(model, seed, cfg, sampler_name, scheduler, positive, negative,
                                          latent_image_batch, denoise=denoise, alpha_1=alpha_1, reverse_input_batch=True,
                                          blending_schedule=blending_schedule,
                                          stop_blending_at_pct=stop_blending_at_pct,
@@ -847,7 +1114,9 @@ class IterativeMixingKSamplerSimple:
                                                  scheduler, steps,
                                                  0, steps,
                                                  latent_image, normalize=normalize_on_mean)
-        (z_out, _, _, _) = iterative_mixing_ksampler(model, seed, cfg, sampler_name, scheduler, positive, negative,
+        
+        sampler = IterativeMixingKSampler()
+        (z_out, _, _, _) = sampler(model, seed, cfg, sampler_name, scheduler, positive, negative,
                                                   z_primes, denoise=denoise, alpha_1=alpha_1, reverse_input_batch=True,
                                                   blending_schedule=blending_schedule,
                                                   blending_function=blending_function,
@@ -940,6 +1209,7 @@ NODE_CLASS_MAPPINGS = {
     "Latent Batch Statistics Plot": LatentBatchStatisticsPlot,
     "Latent Batch Comparison Plot": LatentBatchComparator,
     "Iterative Mixing KSampler Advanced": IterativeMixingKSamplerAdv,
+    "Iterative Mixing KSampler Inline": IterativeMixingKSamplerInline,
     "Iterative Mixing KSampler": IterativeMixingKSamplerSimple
 }
 
@@ -948,6 +1218,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Batch Unsampler": "Batch Unsampler",
     "Latent Batch Statistics Plot": "Latent Batch Statistics Plot",
     "Iterative Mixing KSampler Advanced": "Iterative Mixing KSampler Advanced",
+    "Iterative Mixing KSampler Inline": "Iterative Mixing KSampler Inline",
     "Iterative Mixing KSampler": "Iterative Mixing KSampler",
     "Latent Batch Comparison Plot": "Latent Batch Comparison Plot"
 }
